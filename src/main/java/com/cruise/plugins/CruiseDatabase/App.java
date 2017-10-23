666c580bb85ba42d48d7933bd7533d0b2512ce4b
steve <stevechappell2000@yahoo.com>
2017-10-23 12:16:58 -0700

    init

---
+++ src/main/java/com/cruise/plugins/CruiseDatabase/App.java
@@ -0,0 +1,13 @@
+package com.cruise.plugins.CruiseDatabase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
